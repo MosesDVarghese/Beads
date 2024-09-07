cd0959a31731222a3c3051c83283c5d1e0e89d7c
mosesdvarghese <accydent@gmail.com>
2024-09-07 20:51:19 +0400

    Initial Commit

---
+++ Beads/BeadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeadsApp.swift
+//  Beads
+//
+//  Created by Moses Varghese on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BeadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
