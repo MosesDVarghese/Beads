deee7f27c187b26b81f926d30b008459262f9110
mosesdvarghese <accydent@gmail.com>
2024-09-11 15:59:10 +0400

    add explore view

--- Beads/Core/Explore/View/ExploreView.swift
+++ Beads/Core/Explore/View/ExploreView.swift
@@ -5,4 +5,38 @@
 //  Created by Moses Varghese on 9/11/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ExploreView: View {
+    @StateObject var viewModel = ExploreViewModel()
+    
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.searchableUsers) { user in
+                        NavigationLink(value: user) {
+                            VStack {
+                                UserCell(user: user)
+                                
+                                Divider()
+                                    .padding(.leading, 60)
+                            }
+                            .padding(.vertical, 4)
+                        }
+                    }
+                }
+            }
+            .navigationDestination(for: User.self, destination: { user in
+                ProfileView(user: user)
+            })
+            .navigationTitle("Search")
+            .searchable(text: $viewModel.searchText, prompt: "Search")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
